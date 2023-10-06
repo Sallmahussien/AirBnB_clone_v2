80dcc31114247359d210629986cc1ed36f5c95d7
salma hussien <salmaa.hussien1@gmail.com>
2023-10-06 05:26:24 +0300

    prepare web servers

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -10,7 +10,7 @@ server {
     server_name _;
 
     location / {
-        ry_files ${uri} ${uri}/ =404;
+        try_files ${uri} ${uri}/ =404;
     }
 
     location /redirect_me {
@@ -35,49 +35,53 @@ exec { 'apt update':
 }
 
 # Install Nginx
--> package { 'nginx':
+package { 'nginx':
   ensure  => installed,
   require => Exec['apt update'],
 }
 
 # Allow Nginx through the firewall
--> exec { 'Nginx HTTP':
+exec { 'Nginx HTTP':
   command => 'ufw allow "Nginx HTTP"',
   path    => '/usr/sbin:/usr/bin:/sbin:/bin',
   require => Package['nginx'],
 }
 
--> file { 'data'
+file { '/data'
   ensure  => 'directory',
   owner   => 'ubuntu',
   group   => 'ubuntu',
 }
 
--> file { '/data/web_static/'
+file { '/data/web_static'
   ensure  => 'directory',
   owner   => 'ubuntu',
   group   => 'ubuntu',
+  require => File["/data],
 }
 
--> file { '/data/web_static/releases/'
-  ensure => 'directory',
+file { '/data/web_static/releases'
+  ensure  => 'directory',
   owner   => 'ubuntu',
   group   => 'ubuntu',
+  require => File["/data/web_static"],
 }
 
--> file { '/data/web_static/shared/'
-  ensure => 'directory',
+file { '/data/web_static/shared'
+  ensure  => 'directory',
   owner   => 'ubuntu',
   group   => 'ubuntu',
+  require => File["/data/web_static"],
 }
 
--> file { '/data/web_static/releases/test/'
-  ensure => 'directory',
+file { '/data/web_static/releases/test'
+  ensure  => 'directory',
   owner   => 'ubuntu',
   group   => 'ubuntu',
+  require => File["/data/web_static/releases"],
 }
 
--> file { '/data/web_static/releases/test/index.html'
+file { '/data/web_static/releases/test/index.html'
   ensure  => 'file',
   content => '<!DOCTYPE html>
   <html>
@@ -89,28 +93,29 @@ exec { 'apt update':
   </html>',
   owner   => 'ubuntu',
   group   => 'ubuntu',
+  require => File["/data/web_static/releases/test"],
 }
 
--> file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+file { '/data/web_static/current':
+  ensure  => 'link',
+  target  => '/data/web_static/releases/test',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
 }
 
--> file { '/var/www':
+file { '/var/www':
   ensure  => 'directory',
   require => Package['nginx'],
   notify  => Service['nginx'],
 }
 
--> file { '/var/www/html':
+file { '/var/www/html':
   ensure  => 'directory',
   require => Package['nginx'],
   notify  => Service['nginx'],
 }
 
--> file { '/var/www/html/index.html':
+-file { '/var/www/html/index.html':
   ensure  => 'present',
   content => "Hello World!\n",
   require => Package['nginx'],
@@ -118,7 +123,7 @@ exec { 'apt update':
   replace => 'true',
 }
 
--> file { '/var/www/html/404.html':
+file { '/var/www/html/404.html':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n",
   require => Package['nginx'],
@@ -126,7 +131,7 @@ exec { 'apt update':
   replace => 'true',
 }
 
--> file { '/etc/nginx/sites-available/default':
+file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_config,
   require => Package['nginx'],
@@ -135,8 +140,7 @@ exec { 'apt update':
 }
 
 # Restart Nginx
--> service { 'nginx':
+service { 'nginx':
   ensure  => running,
   enable  => true,
-  require => [Package['nginx'], File['/etc/nginx/sites-available/default']],
 }
