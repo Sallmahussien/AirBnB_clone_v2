920670e55075a4237e48d85221600bd0bc26de1b
salma hussien <salmaa.hussien1@gmail.com>
2023-10-08 08:09:08 +0300

    Deletes out-of-date archives

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Full deployment"""
 
-from fabric.api import env
+from fabric.api import *
 import os
 
 do_pack = __import__('1-pack_web_static').do_pack
@@ -10,12 +10,8 @@ do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 def deploy():
     """Full deployment"""
-    try:
-        files = os.listdir("versions")
-        if len(files) == 1:
-            archive_path = "versions/" + files[0]
-
-    except Exception:
-        archive_path = do_pack()
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
 
     return do_deploy(archive_path)
